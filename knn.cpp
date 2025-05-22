42b57e954f7dc0dd4d487d5b627976f60af25693
kamiltomasiak <kamtomasiak@o2.pl>
2025-05-22 19:05:15 +0200

    Update knn.cpp

--- knn.cpp
+++ knn.cpp
@@ -12,3 +12,20 @@ double euclideanDistance(const Row& a, const Row& b, size_t num_features) {
     }
     return std::sqrt(sum);
 }
+
+KNNModel::KNNModel(unsigned int k_val) : k(k_val) {}
+
+std::vector<Row> train_data;
+std::vector<double> train_target;
+unsigned int num_features = 0;
+
+void KNNModel::fit(const DataFrame& df) {
+    train_data.clear();
+    train_target.clear();
+
+    for (size_t i = 0; i < df.length(); ++i) {
+        train_data.push_back(df.get_data(i));
+        train_target.push_back(df.get_target(i));
+    }
+    num_features = df.get_num_features();
+}
